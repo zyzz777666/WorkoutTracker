0aef75c88c0a1922ec7e457ed955da2f44fd6642
I <bumbum.bug@yandex.ru>
2024-10-15 19:04:06 +0300

    include api router

--- workout-tracker/main.py
+++ workout-tracker/main.py
@@ -6,6 +6,7 @@ from core.config import settings
 from api import router as api_router
 
 app = FastAPI()
+app.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app",
