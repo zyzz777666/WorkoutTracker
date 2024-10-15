6f205ac6d56c1115a8ceb0c9c2c31c27a317284d
I <bumbum.bug@yandex.ru>
2024-10-15 19:10:22 +0300

    install sqlalchemy

--- workout-tracker/main.py
+++ workout-tracker/main.py
@@ -9,7 +9,6 @@ app = FastAPI()
 app.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app",
-                host=settings.run.host,
-                port=settings.run.port,
-                reload=True)
+    uvicorn.run(
+        app="main:app", host=settings.run.host, port=settings.run.port, reload=True
+    )
